7ae86b4764428b0f20f9a45ee2a824ff3ee42566
IshaYokh <yokhannaisha@gmail.com>
2021-05-15 05:17:29 +1000

    Implemented viewPaymentHistoryServlet to display payment history on payment history view

--- src/java/iotbay/g15/controller/ViewPaymentHistoryServlet.java
+++ src/java/iotbay/g15/controller/ViewPaymentHistoryServlet.java
@@ -11,11 +11,34 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import iotbay.g15.model.dao.*;
 import iotbay.g15.model.*;
+import java.util.ArrayList;
 
 /**
  *
  * @author Isha Yokhanna
+ * 
+ * This servlet class will be used to view payment history list for a user
+ * 
  */
-public class ViewPaymentHistoryServlet {
-    
+public class ViewPaymentHistoryServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        HttpSession session = request.getSession();
+        User user = (User)session.getAttribute("user");
+        PaymentDAO paymentDBManager = (PaymentDAO)session.getAttribute("paymentDBManager");
+        PaymentInfoDAO paymentInfoDBManager = (PaymentInfoDAO)session.getAttribute("paymentInfoDBManager");
+        ArrayList<Payment> payments = new ArrayList<>();
+        ArrayList<PaymentInfo> paymentInfos = new ArrayList<>();
+        
+        try{
+            payments = paymentDBManager.listAllPaymentInfos(user.getID());
+            paymentInfos = paymentInfoDBManager.listAllPaymentInfos(user.getID());
+        }catch(SQLException ex){
+            Logger.getLogger(ViewPaymentHistoryServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        session.setAttribute("payments", payments);
+        session.setAttribute("paymentInfos", paymentInfos);
+        request.getRequestDispatcher("searchPaymentHistory.jsp").include(request, response);
+    }
 }
